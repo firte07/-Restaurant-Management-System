b76f98b711840212fc872921c7e93bbd9ba66e9c
firte07 <45260144+firte07@users.noreply.github.com>
2021-03-10 09:35:37 +0200

    Create MenuItem.java

---
+++ src/BusinessLayer/MenuItem.java
@@ -0,0 +1,8 @@
+package BusinessLayer;
+
+public interface MenuItem {
+    public int computePrice();
+    public String getName();
+    public void setPret(int p);
+    public void setDenumire(String d);
+}
